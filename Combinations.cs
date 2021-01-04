0ac9675c0fbbaba4e5649c966b3d477906d24720
Samana <samana@i.ua>
2021-01-04 15:55:48 +0200

    Init

---
+++ Combinations.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace Samana.Algorithms
+{
+
+    public static class Combinations
+    {
+
+        // Возварщает список коллекций со всеми возможными варинатами перестановки в source
+        public static List<T[]> AllCombinations<T>(T[] source)
+        {
+            List<T[]> resultList = new List<T[]>();
+            foo(source, resultList);
+            return resultList;
+
+
+            void foo(T[] originalSource, List<T[]> result, List<int> currentSet = null)
+            {
+                if (currentSet == null) currentSet = new List<int>();
+
+                for (int i = 0; i < originalSource.Length; i++)
+                {
+                    if (!currentSet.Contains(i))
+                    {
+                        List<int> cloneSet = new List<int>(currentSet);
+                        cloneSet.Add(i);
+                        if (cloneSet.Count == originalSource.Length)
+                        {
+                            T[] r = new T[originalSource.Length];
+                            for (int j = 0; j < cloneSet.Count; j++)
+                            {
+                                r[j] = originalSource[cloneSet[j]];
+                            }
+                            result.Add(r);
+                            return;
+                        }
+                        else
+                            foo(originalSource, result, cloneSet);
+                    }
+                    else continue;
+                }
+            }
+        }
+    }
+}
